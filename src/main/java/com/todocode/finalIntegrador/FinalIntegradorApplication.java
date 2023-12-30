a9958e7876fb5e06e061828048b54706d38a5cc0
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2023-12-30 19:40:37 -0300

    Inicializacion del Proyecto 🎉

---
+++ src/main/java/com/todocode/finalIntegrador/FinalIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.finalIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todocode/finalIntegrador/FinalIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.finalIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
