0fa7eba1b7676f932f17a23099d9540a8ad612d1
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2024-01-03 18:37:43 -0300

    Creacion del repository de Venta ✨

---
+++ src/main/java/com/todocode/finalIntegrador/repository/IVentaRepository.java
@@ -0,0 +1,9 @@
+package com.todocode.finalIntegrador.repository;
+
+import com.todocode.finalIntegrador.models.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IVentaRepository extends JpaRepository<Venta,Long> {
+}
