f23c75ebaabf016dbbc4ffc870490fb8d34713f0
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2024-01-03 20:45:59 -0300

    Creacion de endpoints e inyeccion de dependencias ✨

--- src/main/java/com/todocode/finalIntegrador/controllers/ClienteController.java
+++ src/main/java/com/todocode/finalIntegrador/controllers/ClienteController.java
@@ -1,4 +1,8 @@
 package com.todocode.finalIntegrador.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class ClienteController {
+
 }
--- src/main/java/com/todocode/finalIntegrador/controllers/ProductoController.java
+++ src/main/java/com/todocode/finalIntegrador/controllers/ProductoController.java
@@ -1,4 +1,47 @@
 package com.todocode.finalIntegrador.controllers;
 
+import com.todocode.finalIntegrador.models.Producto;
+import com.todocode.finalIntegrador.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ProductoController {
+    @Autowired
+    IProductoService productoService;
+
+    @PostMapping("/productos/crear")
+    public String crearProducto(@RequestBody Producto producto){
+        productoService.crearProducto(producto);
+        return "Producto creado correctamente";
+    }
+
+    @GetMapping("productos")
+    public List<Producto> getProductos(){
+        return productoService.getProductos();
+    }
+
+    @GetMapping("productos/{id}")
+    public Producto getProductoById(@PathVariable Long id){
+        return productoService.getProductoById(id);
+    }
+
+    @DeleteMapping("productos/eliminar/{id}")
+    public String deleteProductoById(@PathVariable Long id){
+        productoService.deleteById(id);
+        return "Producto eliminado correctamente";
+    }
+
+    @PutMapping("productos/editar/{id}")
+    public Producto editProducto(@RequestBody Producto producto) {
+        productoService.editProduct(producto);
+        return productoService.getProductoById(producto.getCodigo_producto());
+    }
+
+    @GetMapping("productos/falta-stock")
+    public List<Producto> getProductosStockMenorA5(){
+        return productoService.stockMenorA5();
+    }
 }
