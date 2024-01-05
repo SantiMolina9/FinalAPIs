5ca53a76773ed0c92be5dcc3c4468b9f62f492a7
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2024-01-05 11:09:21 -0300

    Creacion de endpoints en los controllers de Cliente y Venta ✨

--- src/main/java/com/todocode/finalIntegrador/controllers/ClienteController.java
+++ src/main/java/com/todocode/finalIntegrador/controllers/ClienteController.java
@@ -1,8 +1,43 @@
 package com.todocode.finalIntegrador.controllers;
 
+import com.todocode.finalIntegrador.models.Cliente;
+import com.todocode.finalIntegrador.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClienteController {
+    @Autowired
+    IClienteService clienteService;
+
+    @PostMapping("/clientes/crear")
+    public String crearCliente(@RequestBody Cliente cliente){
+        clienteService.crearCliente(cliente);
+        return "Cliente creado correctamente";
+    }
+
+    @GetMapping("clientes")
+    public List<Cliente> getClientes(){
+        return clienteService.getClientes();
+    }
+
+    @GetMapping("clientes/{id}")
+    public Cliente getClienteById(@PathVariable Long id){
+        return clienteService.getClienteById(id);
+    }
+
+    @DeleteMapping("clientes/eliminar/{id}")
+    public String deleteClienteById(@PathVariable Long id){
+        clienteService.deleteById(id);
+        return "Cliente eliminado correctamente";
+    }
+
+    @PutMapping("clientes/editar")
+    public Cliente editProducto(@RequestBody Cliente cliente) {
+        clienteService.editCliente(cliente);
+        return clienteService.getClienteById(cliente.getId_cliente());
+    }
 
 }
--- src/main/java/com/todocode/finalIntegrador/controllers/VentaController.java
+++ src/main/java/com/todocode/finalIntegrador/controllers/VentaController.java
@@ -1,4 +1,61 @@
 package com.todocode.finalIntegrador.controllers;
 
+import com.todocode.finalIntegrador.models.Producto;
+import com.todocode.finalIntegrador.models.Venta;
+import com.todocode.finalIntegrador.models.dto.VentaDTO;
+import com.todocode.finalIntegrador.service.IVentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
 public class VentaController {
+    @Autowired
+    IVentaService ventaService;
+
+    @PostMapping("/ventas/crear")
+    public String crearVenta(@RequestBody Venta venta){
+        ventaService.crearVenta(venta);
+        return "Venta creada correctamente";
+    }
+
+    @GetMapping("ventas")
+    public List<Venta> getVentas(){
+        var venta = ventaService.getVentas();
+        return venta;
+    }
+
+    @GetMapping("ventas/{id}")
+    public Venta getVentaById(@PathVariable Long id){
+        return ventaService.getVentaById(id);
+    }
+
+    @DeleteMapping("ventas/eliminar/{id}")
+    public String deleteVentaById(@PathVariable Long id){
+        ventaService.deleteById(id);
+        return "Venta eliminada correctamente";
+    }
+
+    @PutMapping("ventas/editar/{id}")
+    public Venta editVenta(@RequestBody Venta venta) {
+        ventaService.editVenta(venta);
+        return ventaService.getVentaById(venta.getCodigo_venta());
+    }
+    @GetMapping("ventas/productos/{codigo_venta}")
+    public List<Producto> getListaProductos(@PathVariable Long codigo_venta){
+        return ventaService.getListaDeProductos(codigo_venta);
+    }
+
+    @GetMapping("ventas/fecha/{fecha}")
+    public String getVentasPorFecha(@PathVariable LocalDate fecha){
+        return ventaService.ventasFecha(fecha);
+    }
+
+    @GetMapping("ventas/mayor_venta")
+    public VentaDTO mayorVenta(){
+        return ventaService.mayorVenta();
+    }
+
 }
