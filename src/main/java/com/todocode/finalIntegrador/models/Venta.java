e906e6eba48dc2ffc2d8f5dee8f06c43d8cc69db
SantiMolina9 <118134456+SantiMolina9@users.noreply.github.com>
2024-01-05 11:04:51 -0300

    Arreglo de las relaciones de JPA 🐛

--- src/main/java/com/todocode/finalIntegrador/models/Venta.java
+++ src/main/java/com/todocode/finalIntegrador/models/Venta.java
@@ -1,19 +1,33 @@
 package com.todocode.finalIntegrador.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo_venta;
+
     private LocalDate fecha_venta;
     private Double total;
+
+    @ManyToMany
+    @JoinTable(name = "venta_lista_productos",
+    joinColumns = {@JoinColumn(name = "codigo_venta")},
+    inverseJoinColumns = {@JoinColumn(name = "codigo_producto")}
+    )
     private List<Producto> lista_productos;
-    private Cliente unCliente;
+
+    @ManyToOne
+    private Cliente cliente;
 }
